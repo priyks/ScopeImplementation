f50a7f5660a6fe7f2b8c2bbb98d55126689d1ccf
priyks <priyankakulkarni3431@gmail.com>
2021-02-19 20:07:52 +0530

    checked bean scope

--- ScopeImplementation/src/main/java/com/stereotype/Student.java
+++ ScopeImplementation/src/main/java/com/stereotype/Student.java
@@ -3,9 +3,11 @@ package com.stereotype;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("studentObj")
+@Scope("prototype")
 public class Student {
     @Value("Priyanka Kulkarni")
 	private String name;
--- ScopeImplementation/src/main/java/com/stereotype/TestStudent.java
+++ ScopeImplementation/src/main/java/com/stereotype/TestStudent.java
@@ -11,8 +11,12 @@ public class TestStudent {
 		  ApplicationContext context=new ClassPathXmlApplicationContext("com/stereotype/sereotypeConfig.xml");
 		  Student student=context.getBean("studentObj",Student.class);
 		  System.out.println(student);
+		  System.out.println(student.hashCode());
 		  System.out.println(student.getPhoneNo());
 		  System.out.println(student.getPhoneNo().getClass().getName());
+		  
+		  Student student2=context.getBean("studentObj",Student.class);
+		  System.out.println(student2.hashCode()); // it will print the same hashcode bcz scope of bean is singleton
 	}
 
 }
@@ -32,4 +36,14 @@ Student [name=Priyanka Kulkarni, city=Pune]
 
 when can also specify name of Bean using @Component("studentObj")
 
+
+prototype:
+
+@Scope("prototype") it will give different object with each request 
+output :
+Student [name=Priyanka Kulkarni, city=Pune]
+400103862
+[9766357093, 9850481457, 8778146161]
+java.util.Vector
+573200870
 */
